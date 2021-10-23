d59283fdd92f7719192d51d5136967262dde2a23
Aleksander Jablonski <aleksander.jablonski@indevops.com>
2021-10-23 22:36:41 +0200

    Add login page and intent to second activity

---
+++ app/src/main/java/com/example/blockchainapp/Login.java
@@ -0,0 +1,55 @@
+package com.example.blockchainapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class Login extends AppCompatActivity {
+
+    private EditText Name;
+    private EditText Password;
+    private TextView Info;
+    private Button Login;
+    private int counter = 5;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        Name = (EditText) findViewById(R.id.etName);
+        Password = (EditText) findViewById(R.id.etPassword);
+        Info = (TextView) findViewById(R.id.tvInfo);
+        Login = (Button) findViewById(R.id.btnLogin);
+
+        Info.setText("Number of attempts remaining: " + String.valueOf(counter));
+
+        Login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                validate(Name.getText().toString(), Password.getText().toString());
+            }
+        });
+    }
+
+    private void validate (String userName, String userPassword){
+        if((userName.equals("admin")) && (userPassword.equals("admin"))){
+            Intent intent = new Intent(Login.this, Choice.class);
+            startActivity(intent);
+        }else{
+            counter--;
+
+            Info.setText("Number of attempts remaining: " + String.valueOf(counter));
+
+            if(counter == 0){
+                Login.setEnabled(false);
+            }
+        }
+    }
+
+}
